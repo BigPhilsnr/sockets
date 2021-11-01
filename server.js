56a8b578b2674ae6a6a04ed185ec443c3e3d6d61
Philip Ademba <philmaxsnr@gmail.com>
2021-11-01 19:24:36 +0300

    updated socket

--- server.js
+++ server.js
@@ -2,20 +2,43 @@ const app = require('express')()
 const http = require('http').createServer(app)
 const connectedUsers =[];
 
+const socketio = require('socket.io')(http);
+socketio.connectedUsers =[];
 
 app.get('/', (req, res) => {
+   var clients =  socketio.connectedUsers;
+   console.log(clients)
     res.send("Connected users"+ JSON.stringify(connectedUsers))
-})
-
-//Socket Logic
-const socketio = require('socket.io')(http)
+});
 
 socketio.on("connection", (userSocket) => {
-    connectedUsers.push[userSocket]
+    console.log('user connected')
+  console.log(userSocket.id)
+ 
     userSocket.emit("receive_message", {first_load:"socket io"})
+
+    userSocket.emit("get_user", {socket_id:userSocket.id})
+
+    userSocket.on('set_user', (data)=>{
+        console.log(data);
+        
+        socketio.connectedUsers= socketio.connectedUsers.filter(element=> element.email !== data.email);
+        socketio.connectedUsers.push(data)
+        socketio.emit('connected_users',{users: socketio.connectedUsers} )
+
+    });
+
     userSocket.on("send_message", (data) => {
         userSocket.broadcast.emit("receive_message", data)
-    })
+    });
+
+    userSocket.on('disconnect', (data)=>{
+        socketio.connectedUsers= socketio.connectedUsers.filter(element=> element.sockeId !== userSocket.id);
+        socketio.emit('connected_users',{users: socketio.connectedUsers} )
+        console.log("disconnected")
+    });
+
+
 })
 
 let port = process.env.PORT || 8080
